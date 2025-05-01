c773cafde567dac7dde0667c535ed258c0388a13
Ana Silva <anajnsilva@gmail.com>
2025-05-01 13:51:14 -0700

    Created index.js with primary server set up, created basic file structutre, created git.ignore file

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const fs = require("fs");
+
+const saltRounds = 12;
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use("/js", express.static(__dirname + "./js"));
+app.use("/css", express.static(__dirname + "./css"));
+app.use("/img", express.static(__dirname + "./img"));
+
+app.use(session({
+    secret: "ed14ad73-6cf4-433e-83bc-93411dfdc2c5",
+    resave: false,
+    saveUninitialized: true
+}))
+
+// Route for landing page pre-login
+app.get("/", function (req, res) {
+    let doc = fs.readFileSync("./html/index.html", "utf8");
+    res.send(doc);
+})
+
+// Route for about page
+app.get("/about", function (req, res) {
+    let doc = fs.readFileSync("./html/about.html", "utf8");
+    res.send(doc);
+})
+
+// Route for login page
+app.get("/login", function (req, res) {
+    let doc = fs.readFileSync("./html/login.html", "utf8");
+    res.send(doc);
+})
+
+// Route for browse page
+app.get("/browse", function (req, res) {
+    let doc = fs.readFileSync("./html/browse.html", "utf8");
+    res.send(doc);
+})
+
+// Route for contents page
+app.get("/contents", function (req, res) {
+    let doc = fs.readFileSync("./html/contents.html", "utf8");
+    res.send(doc);
+})
+
+// Route for directions page
+app.get("/directions", function (req, res) {
+    let doc = fs.readFileSync("./html/directions.html", "utf8");
+    res.send(doc);
+})
+
+// Route for manage page
+app.get("/manage", function (req, res) {
+    let doc = fs.readFileSync("./html/manage.html", "utf8");
+    res.send(doc);
+})
+
+// Route for reviews page
+app.get("/reviews", function (req, res) {
+    let doc = fs.readFileSync("./html/reviews.html", "utf8");
+    res.send(doc);
+})
+
+// Route for profile page
+app.get("/profile", function (req, res) {
+    let doc = fs.readFileSync("./html/profile.html", "utf8");
+    res.send(doc);
+})
+
+// Route for view your fridges page
+app.get("/view-own", function (req, res) {
+    let doc = fs.readFileSync("./html/view_own.html", "utf8");
+    res.send(doc);
+})
+
+// Route for create new fridge/pantry page
+app.get("/new", function (req, res) {
+    let doc = fs.readFileSync("./html/create_new.html", "utf8");
+    res.send(doc);
+})
+
+// Logout user and destroys current session
+app.post("/logout", function (req, res) {
+    if (req.session) {
+        req.session.destroy(function (error) {
+            if (error) {
+                res.status(500).send("Unable to log out")
+            } else {
+                res.redirect("/");
+            }
+        });
+    }
+});
+
+// Page not found
+app.use(function (req, res, next) {
+    let doc = fs.readFileSync("./html/404.html", "utf8");
+    res.status(404).send(doc);
+    next();
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
