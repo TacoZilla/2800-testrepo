351f9ea5721fc053e79f34ca3ed31acb5fbf0a0f
Ana Silva <anajnsilva@gmail.com>
2025-05-02 15:26:38 -0700

    Created .env file and added configs to connect to the aiven database.

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ const express = require("express");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
 const fs = require("fs");
+const pg = require("pg");
+const dotenv = require('dotenv').config();
 
 const saltRounds = 12;
 const app = express();
 const port = process.env.PORT || 8000;
 
+const config = {
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.DBPORT,
+    database: process.env.DATABASE,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync("./ca.pem").toString(),
+    }
+}
+
+const client = new pg.Client(config);
+
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/js", express.static(__dirname + "./js"));
 app.use("/css", express.static(__dirname + "./css"));
