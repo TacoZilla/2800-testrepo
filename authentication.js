40e26fc4e5ad192ea7875964d8696559f14da7cd
Ana Silva <anajnsilva@gmail.com>
2025-05-09 12:47:12 -0700

    Fixed bug with authentication validating passwords still letting users into browse page with invalid password

--- authentication.js
+++ authentication.js
@@ -62,7 +62,7 @@ module.exports = function (app) {
                 console.log(err);
                 return;
             }
-            client.query(`SELECT * FROM "users" WHERE users.email = $1`, [email], (error, results) => {
+            client.query(`SELECT * FROM "users" WHERE users.email = $1`, [email], async (error, results) => {
 
                 if (error) {
 
@@ -76,15 +76,14 @@ module.exports = function (app) {
                     return;
                 }
 
-                let invalidPassword = true;
-                if (bcrypt.compare(password, results.rows[0].password)) {
-                    invalidPassword = false;
-                }
+                let validPassword = await bcrypt.compare(password, results.rows[0].password);
 
-                if (invalidPassword) {
+                if (!validPassword) {
                     res.send({ status: 'fail', msg: 'Invalid password' });
                     return;
                 }
+
+                console.log(validPassword);
 
                 req.session.authenticated = true;
                 req.session.userId = results.rows[0]["userId"];
